f94ebd31b79741fe7cc03a8f71aacdfaaab993a8
Najia Afrin <najiaafrin2@gmail.com>
2019-09-20 20:01:37 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+    items:1,
+    loop:true,
+    // autoWidth:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    autoHeight:true
+    });
+  });
